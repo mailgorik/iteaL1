7bddcc5657632e16176d159e84d5f82cbc6b3fd9
rudi <mailgorik@gmail.com>
2023-08-23 21:31:01 +0300

    adder an object

---
+++ src/p1/Car1.java
@@ -0,0 +1,27 @@
+package p1;
+
+/**
+ *
+ */
+
+public class Car1 {
+    //поля/змінні відповідають за стан класу
+    String color;
+    float weight;
+
+    // методи відповідають за ??
+    // звертати увагу на неймінг!! можуть бути проблеми на code review
+    void printColorAndWeight(){
+        System.out.println(color + " " + weight);
+    }
+
+    static int sumOfTwoNumbers(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+
+    // public - метод доступний для запуску з будь-якого місця
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
